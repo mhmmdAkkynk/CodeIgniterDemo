44f2f4b128b20a930e718938814992b2defb35ce
mhmmdAkkynk <mhmmd_akkynk@hotmail.com>
2021-05-19 12:31:44 +0300

    Database Added!

---
+++ UsersCrud/app/Database/userscrudsample.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 19 May 2021, 11:07:09
+-- Sunucu sürümü: 10.4.18-MariaDB
+-- PHP Sürümü: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `userscrudsample`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL COMMENT 'Primary Key',
+  `name` varchar(100) NOT NULL COMMENT 'Name',
+  `email` varchar(255) NOT NULL COMMENT 'Email Address'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='datatable demo table';
+
+--
+-- Tablo döküm verisi `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`) VALUES
+(1, 'John Doe', 'john@gmail.com'),
+(2, 'Vanya Hargreeves', 'vanya@gmail.com'),
+(3, 'Luther Hargreeves', 'luther@gmail.com'),
+(4, 'Diego Hargreeves', 'diego@gmail.com'),
+(5, 'Klaus Hargreeves', 'klaus@gmail.com'),
+(6, 'Ben Hargreeves', 'ben@gmail.com'),
+(7, 'The Handler', 'handler@gmail.com'),
+(8, 'Muhammed Akkaynak', 'mhmmdakkynk@gmail.com'),
+(10, 'Semih Kaya', 'semih@gmail.com'),
+(12, 'Yeliz Kedi', 'yeliz@gmail.com'),
+(13, 'Mustafa Tekin', 'mustafat@hotmail.com');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key', AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
